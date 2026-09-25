f5dfc19f36ec900aa956dfa3ff4bd7a5e838d8f4
Aarambha Dhakal <shivakdhakal@Aarambhas-MacBook-Pro.local>
2026-09-25 18:05:52 +0100

    passing a value

---
+++ function/04.js
@@ -0,0 +1,27 @@
+function add(a, b) {
+return a + b;
+}
+
+let result = add(10, 20);
+console.log(result);
+
+function subtract(a, b) {
+    return a - b;
+}
+
+let subResult = subtract(20, 10);
+console.log(subResult);
+
+function multiply(a, b) {
+    return a * b;
+}
+
+let multResult = multiply(10, 20);
+console.log(multResult);
+
+function divide(a, b) {
+    return a / b;
+}
+
+let divResult = divide(20, 10);
+console.log(divResult);
