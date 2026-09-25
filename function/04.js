357b8e37d686c67ea313592c88036d2c822d2c5b
Aarambha Dhakal <shivakdhakal@Aarambhas-MacBook-Pro.local>
2026-09-25 18:30:19 +0100

    celcius to farenite

--- function/04.js
+++ function/04.js
@@ -1,27 +1,60 @@
-function add(a, b) {
-return a + b;
+// function add(a, b) {
+// return a + b;
+// }
+
+// let result = add(10, 20);
+// console.log(result);
+
+// function subtract(a, b) {
+//     return a - b;
+// }
+
+// let subResult = subtract(20, 10);
+// console.log(subResult);
+
+// function multiply(a, b) {
+//     return a * b;
+// }
+
+// let multResult = multiply(10, 20);
+// console.log(multResult);
+
+// function divide(a, b) {
+//     return a / b;
+// }
+
+// let divResult = divide(20, 10);
+// console.log(divResult);
+
+
+
+
+
+
+// function fullname(firstname, middlename, lastname){
+//     return firstname + " " + middlename + " " + lastname;
+// }
+
+// let result5 = fullname("Aarambha", "Kumar", "Dhakal");
+// console.log(result5);
+
+
+
+
+
+
+
+
+
+
+
+function celsiusToFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
 }
 
-let result = add(10, 20);
-console.log(result);
+let celsius = 120;
+let fahrenheit = celsiusToFahrenheit(celsius);
+console.log(fahrenheit + "°F"); 
 
-function subtract(a, b) {
-    return a - b;
-}
 
-let subResult = subtract(20, 10);
-console.log(subResult);
 
-function multiply(a, b) {
-    return a * b;
-}
-
-let multResult = multiply(10, 20);
-console.log(multResult);
-
-function divide(a, b) {
-    return a / b;
-}
-
-let divResult = divide(20, 10);
-console.log(divResult);
